b96a195619a56304a2665f038dd92947b7a85acd
manunag <36444982+manunag@users.noreply.github.com>
2018-07-09 17:59:40 +0530

    Update hunter2.py

--- hunter2.py
+++ hunter2.py
@@ -1,12 +1,16 @@
-n=int(input())
-a=list(input()for i in range(n))
+n=input()
+if n.isalpha():
+    print("invalid")
+else:
+    n=int(n)
+    a=list(input()for i in range(n))
 
-for i in range(0,n):
-    
-    for j in range (i+1,n):
-        if(a[i]<a[j]):
-            temp=a[i]
-            a[i]=a[j]
-            a[j]=temp
-for i in a:
-    print(int(i) ,end='')
+    for i in range(0,n):
+
+        for j in range (i+1,n):
+            if(a[i]<a[j]):
+                temp=a[i]
+                a[i]=a[j]
+                a[j]=temp
+    for i in a:
+        print(int(i) ,end='')
