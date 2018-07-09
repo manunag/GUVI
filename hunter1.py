27d540a78bffd8853c98358a71c9b5796fd116a1
manunag <36444982+manunag@users.noreply.github.com>
2018-07-09 17:52:41 +0530

    Update hunter1.py

--- hunter1.py
+++ hunter1.py
@@ -6,16 +6,17 @@ else:
     x=list(x)
     n=int(n)
     count=int(0)
+    c=int(0)
 
     for i in range(0,n):
         if x[i]!=0:
             count=0
             for z in range(i+1,n):
                 if(x[i]==x[z]):
-                    
                     x[z]=0
-                    
                     count=count+1
-                    
             if count!=0:
                 print (int(x[i]))
+                c=c+1
+    if c==0:
+        print("unique")
