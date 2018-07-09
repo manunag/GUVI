1d3ca4511faf28765f3fb2ed3401135321fde8e0
manunag <36444982+manunag@users.noreply.github.com>
2018-07-09 22:12:25 +0530

    Create hunter3.py

---
+++ hunter3.py
@@ -0,0 +1,13 @@
+n=input()
+if n.isalpha():
+    print("invalid")
+else:
+    n=int(n)
+    a = [int(i) for i in input().split()]
+    chk=0
+    for i in range(0,n):
+        if a[i]==i:
+            print(i,end=' ')
+            chk=1
+    if chk==0:
+        print(-1)
