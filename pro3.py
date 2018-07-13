123487284450febc6950b667eb03069f14238cc5
manunag <36444982+manunag@users.noreply.github.com>
2018-07-13 19:55:41 +0530

    Update pro3.py

--- pro3.py
+++ pro3.py
@@ -2,15 +2,28 @@ x,y=input().split()
 lenx=int(len(x))
 len=len(y)
 count=0
-for i in range(0,int(len)):
-    if(i<lenx):
-        if x[i] in y[i]:
-            continue
+if(lenx<=len):
+    for i in range(0,int(len)):
+        if(i<lenx):
+            if x[i] in y[i]:
+                continue
+            else:
+                x=x[:i]+y[i]+x[i+1:]
+                count=count+1
         else:
-            x=x[:i]+y[i]+x[i+1:]
+            x=x[:i+1]+y[i]
             count=count+1
-    else:
-        x=x[:i+1]+y[i]
-        count=count+1
-# x=x[:1]+'z'+x[2:]
+else:
+    count=lenx-int(len)
+
+    x=x[:int(len)]
+
+
+    for i in range(0,int(len)):
+        if lenx>len:
+            if x[i] in y[i]:
+                continue
+            else:
+                x=x[:i]+y[i]+x[i+1:]
+                count=count+1
 print(count)
