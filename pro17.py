36346bc8d0eb565c3c42f219b9e0d6aeb909e12f
manunag <36444982+manunag@users.noreply.github.com>
2018-07-15 18:02:10 +0530

    Update pro17.py

--- pro17.py
+++ pro17.py
@@ -1,16 +1,17 @@
-
 x,y=input().split()
-len=len(x)
+
 x=int(x)
 y=int(y)
 a=[int(i) for i in input().split()]
+len=len(a)
 chk=0
 
-for i in range(0,len):
+for i in range(0,int(len)):
 
-    for j in range(i+1,len):
+    for j in range(i+1,int(len)):
 
         z=int(a[i])+int(a[j])
+        
         if z==y:
             chk=1
 if chk==1:
