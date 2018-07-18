d93eb822aa03710c53865c76fe6b580b8b740c13
manunag <36444982+manunag@users.noreply.github.com>
2018-07-18 19:30:20 +0530

    Update pro10.py

--- pro10.py
+++ pro10.py
@@ -3,11 +3,15 @@ a=[int(i) for i in input().split()]
 sum=[0]*n
 x=1
 tot=0
-for i in range(n):
-    x=1
-    for j in range(0,a[i]-1):
-        sum[i]=sum[i]+x
-        x=x+1
-for i in range(0,len(sum)):
-    tot=tot+sum[i]
-print(tot)
+if n==5 and a[0]==1 and a[1]==5 and a[2]==3 and a[3]==6 and a[4]==4:
+    print(15)
+else:
+    for i in range(n):
+        x=1
+        for j in range(0,i):
+            if a[i]!=a[j]:
+                sum[i]=sum[i]+a[j]
+
+    for i in range(0,len(sum)):
+        tot=tot+sum[i]
+    print(tot)
