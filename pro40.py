22651dd69eb0d14a7cfad644764067da42f762b6
manunag <36444982+manunag@users.noreply.github.com>
2018-07-25 15:19:39 +0530

    Create pro40.py

---
+++ pro40.py
@@ -0,0 +1,16 @@
+
+s=input()
+count=0
+a=['d','h','o','n','i']
+for i in range(0,len(s)):
+    for j in range(0,len(s)):
+        if a[i]==s[j]:
+            a[i]=0
+            break
+for i in range(len(a)):
+    if a[i]!=0:
+        count=1
+if count==0:
+    print("yes")
+else:
+    print("no")
