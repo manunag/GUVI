683dc2cb5820090fdddaeea1e96636102d8ea557
manunag <36444982+manunag@users.noreply.github.com>
2018-07-25 15:25:13 +0530

    Update pro40.py

--- pro40.py
+++ pro40.py
@@ -2,15 +2,18 @@
 s=input()
 count=0
 a=['d','h','o','n','i']
-for i in range(0,len(s)):
-    for j in range(0,len(s)):
-        if a[i]==s[j]:
-            a[i]=0
-            break
-for i in range(len(a)):
-    if a[i]!=0:
-        count=1
-if count==0:
-    print("yes")
+if len(s)==5:
+    for i in range(0,len(s)):
+        for j in range(0,len(s)):
+            if a[i]==s[j]:
+                a[i]=0
+                break
+    for i in range(len(a)):
+        if a[i]!=0:
+            count=1
+    if count==0:
+        print("yes")
+    else:
+        print("no")
 else:
     print("no")
