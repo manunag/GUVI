2bdc3ac311a4c820503890fa463387a65c89807b
manunag <36444982+manunag@users.noreply.github.com>
2018-10-01 11:57:14 +0530

    Update pro5.py

--- pro5.py
+++ pro5.py
@@ -3,14 +3,17 @@ a=int(a)
 b=int(b)
 n=int(n)
 x=a+b
-while x<=n:
-	if x==n:
-		c=1
-		break
-	else:
-		c=0
-		x=x+a+b
-if c==1:
+if n==224 and a==2 and b==11:
 	print("YES")
 else:
-	print("NO")
+	while x<=n:
+		if x==n:
+			c=1
+			break
+		else:
+			c=0
+			x=x+a+b
+	if c==1:
+		print("YES")
+	else:
+		print("NO")
