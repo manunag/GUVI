bde1a58223723658dd7a78428cf053fe53d15b38
manunag <36444982+manunag@users.noreply.github.com>
2018-11-14 17:49:54 +0530

    Create pro59.py

---
+++ pro59.py
@@ -0,0 +1,20 @@
+a = input()
+b = a.split('|')
+c = input()
+
+n = len(b[0])-len(b[1])
+
+if n==0 :
+    print('Impossible')
+    sys.exit()
+if n != len(c) :
+    print('Impossible')
+    sys.exit()
+if len(c) >= n :
+    if len(b[0]) < len(b[1]) :
+        s = b[0] + c + '|' + b[1]
+    else :
+        s = b[0] + '|' + b[1] + c
+    print(s)
+else :
+    print('Impossible')
